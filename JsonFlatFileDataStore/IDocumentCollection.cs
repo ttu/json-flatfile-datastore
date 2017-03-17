c2cda699222fb695cc18d422d69c4b5de08d639d
ttu <tomi.tuhkanen@iki.fi>
2017-03-17 22:28:24 +0200

    Summaries to interface

--- JsonFlatFileDataStore/IDocumentCollection.cs
+++ JsonFlatFileDataStore/IDocumentCollection.cs
@@ -10,48 +10,92 @@ namespace JsonFlatFileDataStore
     /// <typeparam name="T"></typeparam>
     public interface IDocumentCollection<T>
     {
+        /// <summary>
+        /// Collection as queryable
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<T> AsQueryable();
 
+        /// <summary>
+        /// Find all items that match the query
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
         IEnumerable<T> Find(Predicate<T> query);
 
+        /// <summary>
+        /// Inserts a single item
+        /// </summary>
+        /// <param name="entity"></param>
         void InsertOne(T entity);
 
+        /// <summary>
+        /// Inserts a single item
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Task InsertOneAsync(T entity);
 
         /// <summary>
-        /// Replace the first document that matches the filter
+        /// Replace the first item that matches the filter
         /// </summary>
         /// <param name="filter"></param>
         /// <param name="entity"></param>
         void ReplaceOne(Predicate<T> filter, T entity);
 
+        /// <summary>
+        /// Replace the first item that matches the filter
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Task ReplaceOneAsync(Predicate<T> filter, T entity);
 
         /// <summary>
-        /// Update the first document that matches the filter
+        /// Update the first item that matches the filter
         /// </summary>
         /// <param name="filter"></param>
         /// <param name="entity"></param>
         void UpdateOne(Predicate<T> filter, T entity);
 
-        Task UpdateOneAsync(Predicate<T> filter, T entity);
+        /// <summary>
+        /// Update the first item that matches the filter
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        // TODO: Not implemented
+        //Task UpdateOneAsync(Predicate<T> filter, T entity);
 
         /// <summary>
-        /// Delete first that matches the filter
+        /// Delete first item that matches the filter
         /// </summary>
         /// <param name="filter"></param>
         void DeleteOne(Predicate<T> filter);
 
+        /// <summary>
+        /// Delete first item that matches the filter
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         Task DeleteOneAsync(Predicate<T> filter);
+
+        /// <summary>
+        /// Delete all items that match the filter
+        /// </summary>
+        /// <param name="filter"></param>
+        void DeleteMany(Predicate<T> filter);
 
         /// <summary>
         /// Delete all that match the filter
         /// </summary>
         /// <param name="filter"></param>
-        void DeleteMany(Predicate<T> filter);
-
+        /// <returns></returns>
         Task DeleteManyAsync(Predicate<T> filter);
 
+        /// <summary>
+        /// Number of items in the collection
+        /// </summary>
         int Count { get; }
     }
 }
