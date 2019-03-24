c5eda83f40d749542c6459dd8f43f75fb8b54ff2
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2019-03-24 18:29:13 +0200

    Update UTHelpers.cs

--- JsonFlatFileDataStore.Test/UTHelpers.cs
+++ JsonFlatFileDataStore.Test/UTHelpers.cs
@@ -2,12 +2,19 @@
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 
 namespace JsonFlatFileDataStore.Test
 {
     public static class UTHelpers
     {
+        private static string _dir = Path.GetDirectoryName(typeof(DataStoreTests).GetTypeInfo().Assembly.Location);
+
+        private static Lazy<string> _originalContent = new Lazy<string>(() =>
+        {
+            var path = Path.Combine(_dir, "datastore.json");
+            return File.ReadAllText(path);
+        });
+
         public static string Up([CallerMemberName] string name = "")
         {
             var newFilePath = Path.Combine(_dir, $"{name}.json");
@@ -19,13 +26,5 @@ namespace JsonFlatFileDataStore.Test
         {
             File.Delete(fullPath);
         }
-
-        private static string _dir = Path.GetDirectoryName(typeof(DataStoreTests).GetTypeInfo().Assembly.Location);
-
-        private static Lazy<string> _originalContent = new Lazy<string>(() =>
-        {
-            var path = Path.Combine(_dir, "datastore.json");
-            return File.ReadAllText(path);
-        });
     }
 }
