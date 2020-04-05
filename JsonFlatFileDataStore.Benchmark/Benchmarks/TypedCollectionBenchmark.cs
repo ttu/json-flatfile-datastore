2d1f22f174cf50ac3dfcae15d7af983e87107c7c
Krzysztof Kozlowski <krzysztof.kozlowski@pl.whoohoo.com>
2020-04-05 16:24:47 +0200

    Add benchmarks for ITypedCollection methods

--- JsonFlatFileDataStore.Benchmark/Benchmarks/TypedCollectionBenchmark.cs
+++ JsonFlatFileDataStore.Benchmark/Benchmarks/TypedCollectionBenchmark.cs
@@ -67,5 +67,95 @@ namespace JsonFlatFileDataStore.Benchmark
         {
             await _collection.DeleteOneAsync(e => e.Id == 1);
         }
+
+        [Benchmark]
+        public void DeleteMany()
+        {
+            _collection.DeleteMany(e => true);
+        }
+
+        [Benchmark]
+        public async Task DeleteManyAsync()
+        {
+            await _collection.DeleteManyAsync(e => true);
+        }
+
+        [Benchmark]
+        public void ReplaceOne_With_Predicate()
+        {
+            _collection.ReplaceOne(e => e.Id == 1, new User { Id = 1, Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public void ReplaceOne_With_Id()
+        {
+            _collection.ReplaceOne(1, new User { Id = 1, Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public async Task ReplaceOneAsync_With_Predicate()
+        {
+            await _collection.ReplaceOneAsync(e => e.Id == 1, new User { Id = 1, Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public async Task ReplaceOneAsync_With_Id()
+        {
+            await _collection.ReplaceOneAsync(1, new User { Id = 1, Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public void ReplaceMany()
+        {
+            _collection.ReplaceMany(e => true, new User { Id = 1, Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public async Task ReplaceManyAsync()
+        {
+            await _collection.ReplaceManyAsync(e => true, new User { Id = 1, Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public void UpdateOne()
+        {
+            _collection.UpdateOne(e => e.Id == 1, new User { Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public async Task UpdateOneAsync()
+        {
+            await _collection.UpdateOneAsync(e => e.Id == 1, new User { Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public void UpdateMany()
+        {
+            _collection.UpdateMany(e => true, new User { Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public async Task UpdateManyAsync()
+        {
+            await _collection.UpdateManyAsync(e => true, new User { Name = "Teddy" });
+        }
+
+        [Benchmark]
+        public void Find_With_Predicate()
+        {
+            _collection.Find(e => e.Id == 1);
+        }
+
+        [Benchmark]
+        public void Find_With_Text()
+        {
+            _collection.Find("James");
+        }
+
+        [Benchmark]
+        public void GetNextIdValue()
+        {
+            _collection.GetNextIdValue();
+        }
     }
 }
