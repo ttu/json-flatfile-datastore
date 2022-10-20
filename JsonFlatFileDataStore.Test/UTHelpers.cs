fec1fe2faa579b25d204ce034b61e73c880fbf36
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-10-20 08:35:11 +0300

    Add test for when creating a new file on DataStore init (#74)

--- JsonFlatFileDataStore.Test/UTHelpers.cs
+++ JsonFlatFileDataStore.Test/UTHelpers.cs
@@ -8,22 +8,24 @@ namespace JsonFlatFileDataStore.Test
     public static class UTHelpers
     {
         private static readonly string _dir = Path.GetDirectoryName(typeof(DataStoreTests).GetTypeInfo().Assembly.Location);
-        private static readonly Aes256 _aes256 = new Aes256();
+        private static readonly Aes256 _aes256 = new();
 
-        private static readonly Lazy<string> _originalContent = new Lazy<string>(() =>
+        private static readonly Lazy<string> _originalContent = new(() =>
         {
             var path = Path.Combine(_dir, "datastore.json");
             return File.ReadAllText(path);
         });
 
+        public static string GetFullFilePath(string name) => Path.Combine(_dir, $"{name}.json");
+
         public static string Up([CallerMemberName] string name = "", string encryptionKey = null)
         {
-            var newFilePath = Path.Combine(_dir, $"{name}.json");
+            var newFilePath = GetFullFilePath(name);
             var dbContent = string.IsNullOrEmpty(encryptionKey) ? _originalContent.Value : _aes256.Encrypt(_originalContent.Value, encryptionKey);
             File.WriteAllText(newFilePath, dbContent);
             return newFilePath;
         }
-
+        
         public static void Down(string fullPath)
         {
             File.Delete(fullPath);
