2a6b299776cac5664dd3e9721af8622151b607c1
ttu <tomi.tuhkanen@iki.fi>
2024-10-31 18:18:15 +0200

    test: combine single item and single item secure tests

--- JsonFlatFileDataStore.Test/SingleItemTests.cs
+++ JsonFlatFileDataStore.Test/SingleItemTests.cs
@@ -8,12 +8,23 @@ namespace JsonFlatFileDataStore.Test
 {
     public class SingleItemTests
     {
-        [Fact]
-        public void GetItem_DynamicAndTyped()
-        {
-            var newFilePath = UTHelpers.Up();
+        private const string EncryptionPassword = "t3stK3y139";
 
-            var store = new DataStore(newFilePath);
+        private (string, DataStore) InitializeFileAndStore(string encryptionKey = null, bool reloadBeforeGetCollection = false)
+        {
+            var newFilePath = string.IsNullOrEmpty(encryptionKey) ? UTHelpers.Up() : UTHelpers.Up(encryptionKey: encryptionKey);
+            var store = new DataStore(newFilePath, encryptionKey: encryptionKey, reloadBeforeGetCollection: reloadBeforeGetCollection);
+            return (newFilePath, store);
+        }
+        
+        private DataStore CreateStore(string filePath, string encryptionKey = null) => new DataStore(filePath, encryptionKey: encryptionKey);
+        
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void GetItem_DynamicAndTyped(string encryptionPassword)
+        {
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var itemDynamic = store.GetItem("myUser");
             var itemTyped = store.GetItem<User>("myUser");
@@ -27,12 +38,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void GetItem_DynamicAndTyped_SimpleType()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void GetItem_DynamicAndTyped_SimpleType(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var itemDynamic = store.GetItem("myValue");
             var itemTyped = store.GetItem<double>("myValue");
@@ -43,12 +54,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void GetItem_DynamicAndTyped_ArrayType()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void GetItem_DynamicAndTyped_ArrayType(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var itemDynamic = store.GetItem("myValues");
             var itemTyped = store.GetItem<List<double>>("myValues");
@@ -61,12 +72,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void GetItem_DynamicAndTyped_DateType()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void GetItem_DynamicAndTyped_DateType(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var test = DateTime.Now.ToShortDateString();
 
@@ -82,12 +93,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void GetItem_Nullable_NotFound()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void GetItem_Nullable_NotFound(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = store.GetItem<int?>("notFound");
             Assert.False(result.HasValue);
@@ -100,12 +111,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void NotFound_Exception()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void NotFound_Exception(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             Assert.Throws<KeyNotFoundException>(() => store.GetItem<User>("notFound"));
             Assert.Throws<KeyNotFoundException>(() => store.GetItem<Guid>("notFound"));
@@ -116,12 +127,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void InsertItem_TypedUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void InsertItem_TypedUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = store.InsertItem("myUser2", new User { Id = 12, Name = "Teddy" });
             Assert.True(result);
@@ -129,7 +140,7 @@ namespace JsonFlatFileDataStore.Test
             var user = store.GetItem<User>("myUser2");
             Assert.Equal("Teddy", user.Name);
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var user2 = store2.GetItem<User>("myUser2");
             Assert.Equal("Teddy", user2.Name);
@@ -140,12 +151,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task InsertItem_DynamicUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task InsertItem_DynamicUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.InsertItemAsync("myUser2", new { id = 12, name = "Teddy" });
             Assert.True(result);
@@ -153,7 +164,7 @@ namespace JsonFlatFileDataStore.Test
             var user = store.GetItem("myUser2");
             Assert.Equal("Teddy", user.name);
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var user2 = store2.GetItem("myUser2");
             Assert.Equal("Teddy", user2.name);
@@ -161,12 +172,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task UpdateItem_DynamicUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task UpdateItem_DynamicUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.InsertItemAsync("myUser2", new { id = 12, name = "Teddy", Value = 2.1 });
             Assert.True(result);
@@ -177,7 +188,7 @@ namespace JsonFlatFileDataStore.Test
 
             var updateResult = await store.UpdateItemAsync("myUser2", new { name = "Harold" });
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var user2 = store2.GetItem("myUser2");
             Assert.Equal("Harold", user2.name);
@@ -185,12 +196,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task UpdateItem_TypedUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task UpdateItem_TypedUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.InsertItemAsync("myUser2", new User { Id = 12, Name = "Teddy" });
             Assert.True(result);
@@ -200,7 +211,7 @@ namespace JsonFlatFileDataStore.Test
 
             var updateResult = await store.UpdateItemAsync("myUser2", new { name = "Harold" });
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var user2 = store2.GetItem<User>("myUser2");
             Assert.Equal("Harold", user2.Name);
@@ -208,12 +219,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task UpdateItem_ValueType()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task UpdateItem_ValueType(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.InsertItemAsync("counter", 2);
             Assert.True(result);
@@ -224,7 +235,7 @@ namespace JsonFlatFileDataStore.Test
             var updateResult = await store.UpdateItemAsync("counter", "2");
             Assert.True(result);
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var c2 = store2.GetItem("counter");
             Assert.Equal("2", c2);
@@ -232,12 +243,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task ReplaceItem_ValueType()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task ReplaceItem_ValueType(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.ReplaceItemAsync("counter", 2);
             Assert.False(result);
@@ -251,7 +262,7 @@ namespace JsonFlatFileDataStore.Test
             var updateResult = await store.ReplaceItemAsync<string>("counter", "2");
             Assert.True(updateResult);
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var c2 = store2.GetItem("counter");
             Assert.Equal("2", c2);
@@ -265,12 +276,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task DeleteItem_DynamicUser_NotFound()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task DeleteItem_DynamicUser_NotFound(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath, reloadBeforeGetCollection: true);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword, reloadBeforeGetCollection: true);
 
             var result = await store.DeleteItemAsync("myUser2");
             Assert.False(result);
@@ -278,12 +289,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task UpdateItem_DynamicUser_NotFound()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task UpdateItem_DynamicUser_NotFound(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath, reloadBeforeGetCollection: true);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword, reloadBeforeGetCollection: true);
 
             var result = await store.UpdateItemAsync("myUser2", new { name = "James" });
             Assert.False(result);
@@ -291,12 +302,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task ReplaceItem_DynamicUser_NotFound()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task ReplaceItem_DynamicUser_NotFound(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.ReplaceItemAsync("myUser2", new { id = 2, name = "James" });
             Assert.False(result);
@@ -304,12 +315,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task ReplaceItem_DynamicUser_Upsert()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task ReplaceItem_DynamicUser_Upsert(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.ReplaceItemAsync("myUser2", new { id = 2, name = "James" }, true);
             Assert.True(result);
@@ -320,12 +331,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task ReplaceItem_TypedUser_Upsert()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task ReplaceItem_TypedUser_Upsert(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = await store.ReplaceItemAsync("myUser2", new User { Id = 2, Name = "James" }, true);
             Assert.True(result);
@@ -336,12 +347,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task ReplaceItem_DynamicUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task ReplaceItem_DynamicUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = store.InsertItem("myUser2", new { id = 12, name = "Teddy" });
             Assert.True(result);
@@ -355,12 +366,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public async Task ReplaceItem_TypedUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public async Task ReplaceItem_TypedUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword);
 
             var result = store.InsertItem<User>("myUser2", new User { Id = 12, Name = "Teddy" });
             Assert.True(result);
@@ -374,12 +385,12 @@ namespace JsonFlatFileDataStore.Test
             UTHelpers.Down(newFilePath);
         }
 
-        [Fact]
-        public void DeleteItem_DynamicUser()
+        [Theory]
+        [InlineData(null)]
+        [InlineData(EncryptionPassword)]
+        public void DeleteItem_DynamicUser(string encryptionPassword)
         {
-            var newFilePath = UTHelpers.Up();
-
-            var store = new DataStore(newFilePath, reloadBeforeGetCollection: true);
+            var (newFilePath, store) = InitializeFileAndStore(encryptionPassword, reloadBeforeGetCollection: true);
 
             var user = store.GetItem("myUser2");
             Assert.Null(user);
@@ -390,7 +401,7 @@ namespace JsonFlatFileDataStore.Test
             user = store.GetItem("myUser2");
             Assert.Equal("Teddy", user.name);
 
-            var store2 = new DataStore(newFilePath);
+            var store2 = CreateStore(newFilePath, encryptionPassword);
 
             var deleteResult = store2.DeleteItem("myUser2");
             Assert.True(deleteResult);
